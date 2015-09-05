53429ebabe5a7d85466e3b43ad8a924b4e2a7d07
Jeffrey Mason <jeffreymason@jeffreys-mbp.attlocal.net>
2015-09-05 09:25:19 -0500

    Initial commit

---
+++ practice3.cpp
@@ -0,0 +1,49 @@
+/*
+COURSE: CSC 125, assignment #3
+PROGRAMMER: JEFFREY MASON
+LAST MODIFIED DATE: 26 August 2015
+DESCRIPTION:  This program is artistic
+FILES: practice3.cpp
+IDE/COMPILER: Microsoft Visual Studio 2013
+INSTRUCTION FOR COMPILATION AND EXECUTION:
+1. double-click on PracticeProj.sln
+2. press CTRL+F7 to COMPILE
+3. press CTRL+SHIFT+B to BUILD (COMPILE+LINK)
+4. press CTRL+F5 to EXECUTE
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << endl << endl << endl;
+	cout << " _____________________" << "      /_____       _____\\ " << endl;
+	cout << " \\                   /" << "      (o____)     (o____)         " << endl;
+	cout << " /                   \\" << "             |  |" << endl;
+	cout << "/_____________________\\" << "            |  |" << endl;
+	cout << "|         _____    O  |" << "            |  | " << endl;
+	cout << "|     |\\_/    .\\  o   |" << "           C    C" << endl;
+	cout << "|     | _   ((  |'    |" << "        " << endl;
+	cout << "|     |/ \\__v__/      |" << "        \\__________/  " << endl;
+	cout << "|                     |" << "       " << endl;
+	cout << "|                     |" << "       " << endl;
+	cout << "|                     |" << endl;
+	cout << "|                     |" << endl;
+	cout << " \\____________________/"<<endl<<endl<<endl<<endl;
+	cout << "========================"<<endl<<endl<<endl;
+	cout << " _____________________" <<  "     / _____       _____ \\"<<    endl;
+	cout << " \\                   /" << "      (o____)     (o____)         "<< endl;
+	cout << " /        _____      \\" << "             |  |"<<endl;
+	cout << "/_____|\\_/  ^  \\ _____\\" <<"            |  |"<< endl;
+	cout << "|     | _   (( x|     |" << "            |  | "<< endl;
+	cout << "|     |/ \\_____/      |" <<"           C    C"<< endl;
+	cout << "|                     |" << "         __________"<< endl;
+	cout << "|                     |" << "        /uuuuuuuuuu\\   "<<endl;
+	cout << "|                     |" << "       /            \\"<< endl;
+	cout << "|                     |" << "       |_nnnnnnnnnn_|"<<endl;
+	cout << "|                     |" << endl;
+	cout << "|                     |" << endl;
+	cout << " \\____________________/" << endl;
+
+	return 0;
+}
