7a71d607193fc6f8fa9c74c6fae3480a19e4fa08
jmason827 <jeffdavidmason@gmail.com>
2015-11-14 18:51:10 -0600

    Update chargedUp.sh

--- chargedUp.sh
+++ chargedUp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 maxcapacity=$(ioreg -l | grep -i maxcapacity | cut -d "=" -f 2 )
+percent=0.95
+limit=$(echo "$percent * $maxcapacity" | bc -l | cut -d "." -f 1 )
 
 while true;
 do
 
 currentcapacity=$(ioreg -l | grep -i maxcapacity | cut -d "=" -f 2 )
 
-if [ $currentcapacity == $maxcapacity ];
+if [ $currentcapacity -gt $limit ];
 then
-mplayer ./Music/ChargedUp.m4a
+mplayer ./Music/chime.wav
 fi
 
 sleep 30
